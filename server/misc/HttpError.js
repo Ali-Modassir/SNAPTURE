948f190eef5774a50f8157496dbcff3999427a17
Modassir Ali <alimodassir@gmail.com>
2021-07-12 15:58:56 +0530

    completed phase-1-frontend

--- server/misc/HttpError.js
+++
@@ -1,9 +0,0 @@
-//Helper funtion for Http Errors
-class HttpError extends Error {
-  constructor(message, errorCode) {
-    super(message);
-    this.code = errorCode;
-  }
-}
-
-module.exports = HttpError;
