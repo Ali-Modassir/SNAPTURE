0cfccda5d76b99e7b8b7421d34dd2278d93a78f2
Modassir Ali <alimodassir@gmail.com>
2021-08-05 11:34:58 +0530

    fixed git-branch-model

--- server/routes/newsRoutes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const newsController = require("../controller/newsController");
-
-const router = Router();
-
-router.get("/:type", newsController.getNews);
-
-module.exports = router;
